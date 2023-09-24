369f73365fc003f4018e63d8aacb8018aaab62d6
zdekra <zdkr@seznam.cz>
2023-09-24 22:03:57 +0200

    odstranění komentářů

--- text analyser.py
+++ text analyser.py
@@ -34,14 +34,6 @@ jedna = list()
 for slovo in ((TEXTS)[zadane_cislo]).split():
     jedna.append(slovo.strip(",.:;"))
 
-# dva = list()
-# for slovo in ((TEXTS)[1]).split():
-#     dva.append(slovo.strip(",.:;"))
-
-# tri = list()
-# for slovo in ((TEXTS)[2]).split():
-#     tri.append(slovo.strip(",.:;"))
-
 if cislo_textu == str(cislo_textu):
     numeric_sum = 0 #součet 
     number = 0
@@ -72,66 +64,7 @@ if cislo_textu == str(cislo_textu):
     delka_slova = []
     for slovo in jedna:
         delka_slova.append(len(slovo))   
-# elif cislo_textu == str(2):
-#     numeric_sum = 0
-#     number = 0
-#     pocet_slov = 0
-#     capitall = int()
-#     uppercase = int()
-#     lowercase = int()
-#     for item in dva:
-#         pocet_slov += 1
-#     print("There are", pocet_slov, "words in the selected text.")
-#     for item in jedna:
-#         if item[0].isupper():
-#             capitall += 1
-#     print("There are", capitall, "titlecase words.")
-#     for item in jedna:
-#         if item[::].isupper() and item[::].isalpha():
-#             uppercase += 1
-#     print("There are", uppercase, "uppecase words.")
-#     for item in jedna:
-#         if item[::].islower() and item[::].isalpha:
-#             lowercase += 1
-#     print("There are", lowercase, "lowercase words.")
-#     for item in dva:
-#         if item.isnumeric():
-#             numeric_sum += int(item)
-#             number += 1
-#     print("There is",number,"numeric string.","\n""The sum of all the numbers",numeric_sum,"\n")
-#     delka_slova = []
-#     for slovo in dva:
-#         delka_slova.append(len(slovo))
-# elif cislo_textu == str(3):
-#     numeric_sum = 0
-#     number = 0
-#     pocet_slov = 0
-#     capitall = int()
-#     uppercase = int()
-#     lowercase = int()
-#     for item in tri:
-#         pocet_slov += 1
-#     print("There are", pocet_slov, "words in the selected text.")
-#     for item in tri:
-#         if item[0].isupper():
-#             capitall += 1
-#     print("There are", capitall, "titlecase words.")
-#     for item in tri:
-#         if item[::].isupper():
-#             uppercase += 1
-#     print("There are", uppercase, "uppercase words.")
-#     for item in tri:
-#         if item[::].islower():
-#             lowercase += 1
-#     print("There are", lowercase, "lowercase words.")
-#     for item in tri:
-#         if item.isnumeric():
-#             numeric_sum += int(item)
-#             number += 1
-#     print("There are",number,"numeric strings.","\n""The sum of all the numbers",numeric_sum,"\n")
-#     delka_slova = []
-#     for slovo in tri:
-#         delka_slova.append(len(slovo))
+
 else: 
     print("Nezadáno správné číslo textu.")
     exit()
